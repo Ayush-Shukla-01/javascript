5e847995021e036c7c7eb49ee12f53d9e960304b
Ayush Shukla <139546234+Ayush-Shukla-01@users.noreply.github.com>
2025-08-04 11:58:13 +0000

    commit

---
+++ 01_basicss/02_dataTypes.js
@@ -0,0 +1,19 @@
+/*score = "33"
+console.log(typeof score);
+let valueInNumber= Number (score)
+console.log(typeof(valueInNumber))
+ 
+
+__from boolean to number 
+
+let isloggedin = false
+let valueinNumber = Number (isloggedin)
+console.log(valueinNumber)
+console.log(typeof valueinNumber)*/
+
+// from number to boolean
+
+ let Score = 1
+ let isboolean = Boolean(Score)
+ console.log(isboolean)
+ console.log(typeof isboolean)
---
+++ 01_basicss/variable.js
@@ -0,0 +1,8 @@
+// console.log("ayush")
+/* console is used for printing*/
+const accountId=12323
+let accountname= "ayush"
+var accountPass = 123452
+
+console.log(accountname);
+console.table([accountId,accountname,accountPass])
